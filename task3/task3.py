5a28d9e3e67e07f26e11ce2fe96cfb369484c1c6
unknown <celestepalet@gmail.com>
2022-11-15 18:21:22 -0300

    1st commit-lower letters

--- task3/task3.py
+++ task3/task3.py
@@ -2,40 +2,37 @@ import os
 import csv
 import shutil
 
-
-#leer el csv y poner en listas su contenido
+# leer el csv y poner en listas su contenido
 with open('students.csv') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    list_students_names=[]
-    list_students_id=[]
-    for row in spamreader:
-        student=' '.join(row).split(',')
+    students_file = csv.reader(csvfile,delimiter=' ')  # separa el csv en lista, separa los elementos d ela lista cuando hay espacio vacio
+    list_students_names = []
+    list_students_id = []
+    for row in students_file:  # split hace la lista separando cuando hay ,
+        student = '_'.join(row).lower().split(',')  # join junta todo en un str, a los elementos de la lista los separa con espacio vacio
         list_students_names.append(student[0])
         list_students_id.append(student[1])
 
-#eliminar primer valor
+# eliminar primer valor
 list_students_names.pop(0)
 list_students_id.pop(0)
 
-#crear carpetas
+# crear carpetas
 for name in list_students_names:
-    if os.path.exists(name)==False:
+    if os.path.exists(name) == False:
         os.mkdir(name)
 
-#datos de rutas
+# datos de rutas
 origin = './data'
-destination_pdf = './Tutorials'
-destination_directories = "./Games"
+destination_pdf = './tutorials'
+destination_directories = "./games"
 
-#lista con lo que contiene la carpeta
+# lista con lo que contiene la carpeta
 files = os.listdir(origin)
 
-#mover archivos
-for file in files:
-    if os.path.isdir(os.path.join(origin,file)):
-        new_path= shutil.move(f"{origin}/{file}", destination_directories)
-        print(f'The directory {file} is now in {destination_directories}')
+# mover archivos
+for file in files:  # path.join une dos los argumentos separando por /
+    if os.path.isdir(
+            os.path.join(origin, file)):  # path.isdir pregunta si el argumento es una carpeta, tengo que darle una ruta
+        new_path = shutil.move(f"{origin}/{file}", destination_directories)  # (ruta del archivo , ruta destino)
     elif os.path.isfile(os.path.join(origin, file)) and file.endswith('.pdf'):
-        new_path= shutil.move(f"{origin}/{file}", destination_pdf)
-        print(f'The file {file} is now in {destination_pdf}')
-        
+        new_path = shutil.move(f"{origin}/{file}", destination_pdf)
